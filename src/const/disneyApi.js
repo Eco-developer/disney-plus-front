1ff387da72969f613f1c64f0d90451ed87801387
Jose Quesada <josesk8mqc@gmail.com>
2023-09-30 16:14:28 -0400

    cambiar la api_url para usar .env.

--- src/const/disneyApi.js
+++ src/const/disneyApi.js
@@ -1,4 +1,2 @@
-//const DISNEY_API = 'https://disney-plus-demo.herokuapp.com/api/v1/';
-
-const DISNEY_API = 'http://localhost:4040/api/v1/';
+const DISNEY_API = process.env.REACT_APP_DISNEYPLUS_API;
 export default DISNEY_API;
